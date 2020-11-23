ef1f4b8cd01ea9ce690d8ee9685ad4c3f9b176cd
Vinh <vinh.giang@pushlishone.nl>
2020-11-23 20:12:32 +0700

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((_, idx) => idx < 4)
-        .map(({ id, ...ortherItemProps }) => (
-          <CollectionItem key={id} {...ortherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...ortherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...ortherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...ortherProps}
   >
     {children}
