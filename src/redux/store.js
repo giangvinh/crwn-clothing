a826c0d6864e643146c74b7edf47cc70e7856232
Vinh <vinh.giang@pushlishone.nl>
2020-11-25 00:45:31 +0700

    update store data normalization, create collection page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
-import rootReducer from './root-reducer';
+import rootReducer from './root.reducer';
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+const reduxStore = { store, persistor };
+
+export default reduxStore;
